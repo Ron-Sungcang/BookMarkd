0427a7c736bcbadb208ea181d0676499d1b5ba22
Ron-Sungcang <sungcangjoseph@gmail.com>
2024-02-17 04:41:39 -0600

    Finished BookRepository

--- app/src/main/java/com/example/bookmarkd/data/BookRepository.kt
+++ app/src/main/java/com/example/bookmarkd/data/BookRepository.kt
@@ -1,6 +1,9 @@
 package com.example.bookmarkd.data
 
+import android.accounts.NetworkErrorException
 import com.example.bookmarkd.model.Book
+import com.example.bookmarkd.network.BookApiService
+
 
 /**
  * Repository retrieves book data from underlying data source.
@@ -13,6 +16,35 @@ interface BookRepository{
 /**
  * Network Implementation of repository that retrieves book data from underlying data source.
  */
-class DefaultBookRepository(){
+class DefaultBookRepository(
+    private  val bookApiService: BookApiService
+): BookRepository {
+    override suspend fun getBook(id: String): Book? {
+        return try {
+            val res = bookApiService.getBook(id)
+            if (res.isSuccessful) {
+                res.body()
+            } else {
+                throw NetworkErrorException(res.code().toString())
+            }
+        } catch (e: Exception){
+            e.printStackTrace()
+            null
+        }
+    }
+
+    override suspend fun getBooks(bookname: String): List<Book>? {
+        return try {
+            val res = bookApiService.getBooks(bookname)
+            if(res.isSuccessful){
+                res.body()?.items ?: emptyList()
+            }else{
+                throw NetworkErrorException(res.code().toString())
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
 
 }
