682032f9392759ed41385f15b18fb9c1690d7ba4
Ron-Sungcang <sungcangjoseph@gmail.com>
2024-02-17 20:48:52 -0600

    Added starter codes for BookListViewModel

--- app/src/main/java/com/example/bookmarkd/screens/BookViewModel.kt
+++ app/src/main/java/com/example/bookmarkd/screens/BookViewModel.kt
@@ -1,6 +1,26 @@
 package com.example.bookmarkd.screens
 
+import android.net.http.HttpException
+import android.os.Build
+import android.text.Spannable.Factory
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.bookmarkd.data.BookRepository
+import com.example.bookmarkd.data.DefaultBookRepository
 import com.example.bookmarkd.model.Book
+import kotlinx.coroutines.launch
+import java.io.IOException
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import com.example.bookmarkd.BookApplication
+
 
 sealed interface BookListUiState{
     data class Success(val books: List<Book>): BookListUiState
@@ -14,3 +34,39 @@ sealed interface BookUiState{
     object Loading: BookUiState
 }
 
+class BookListViewModel(private val bookListRepository: BookRepository): ViewModel()
+{
+    var bookListUiState: BookListUiState by mutableStateOf(BookListUiState.Loading)
+        private set
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getBooks(query: String = "jazz"){//have it as jazz right now until i get the search functions implemented
+        viewModelScope.launch{
+            bookListUiState = BookListUiState.Loading
+            bookListUiState = try{
+                val books = bookListRepository.getBooks(query)
+                if (books == null) {
+                    BookListUiState.Error
+                }else if (books.isEmpty()) {
+                    BookListUiState.Success(emptyList())
+                }else {
+                    BookListUiState.Success(books)
+                }
+            } catch (e: IOException){
+                BookListUiState.Error
+            } catch (e: HttpException){
+                BookListUiState.Error
+            }
+        }
+    }
+
+    companion object{
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as BookApplication)
+                val bookRepository = application.container.bookRepository
+                BookListViewModel(bookRepository)
+            }
+        }
+    }
+}
